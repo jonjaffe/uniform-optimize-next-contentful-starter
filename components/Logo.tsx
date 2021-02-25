2a80a9312cc9ae670724719b0eb11424991a96e7
jonjaffe <jjaffe25@gmail.com>
2021-02-25 13:20:22 -0500

    minor changes

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Logo = () => {
   return (
-    <img src="https://myfopinion.files.wordpress.com/2019/11/new-my-f-opinion-macys-logo-2.jpg">
+    <img src="https://myfopinion.files.wordpress.com/2019/11/new-my-f-opinion-macys-logo-2.jpg" />
   );
 };
 
